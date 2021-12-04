1fdba150ceacb17660f33577eb7c11118efdd61c
Cagdas Salur <cagdassalur@gmail.com>
2021-12-04 09:03:06 +0300

    Add day 4 part 2

---
+++ day_4/part2.py
@@ -0,0 +1,19 @@
+input = open('input').read().strip()
+
+nums = input.split('\n')[0].split(',')
+boards = [[list(filter(None, l.split(' '))) for l in i.split('\n')] for i in input.split('\n\n')[1:]]
+won_boards = []
+combinations = lambda a: list(map(list, zip(*a[::-1])))+a
+
+result = None
+for num in nums:
+  boards = [[['X' if num==c else c for c in row] for row in board] for board in boards if (board not in won_boards)]
+  for board in boards:
+    if ['X']*5 in combinations(board):
+      won_boards.append(board)
+      if len(boards) == 1:
+       result = sum([int(n) for n in sum(board, []) if n != 'X'])*int(num)
+       break
+  if result: break
+
+print(result)
