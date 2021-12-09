8e1a0ac30095f04a5c4f1e85c42d6742991a380f
Cagdas Salur <cagdassalur@gmail.com>
2021-12-09 12:17:12 +0300

    Remove line

--- 2021/day_9/part_2.py
+++ 2021/day_9/part_2.py
@@ -1,7 +1,6 @@
 import collections,math
 
-data = [(100, 'input'), (10, 'testinput')][0]
-area = [[int(cell) for cell in '9'+row+'9'] for row in ('9'*data[0] + '\n' + open(data[1]).read().strip() + '\n' + '9'*data[0]).split('\n')]
+area = [[int(cell) for cell in '9'+row+'9'] for row in ('9'*100 + '\n' + open('input').read().strip() + '\n' + '9'*100).split('\n')]
 
 get_adj = lambda x,y,ar: [ar[y-1][x], ar[y+1][x], ar[y][x-1], ar[y][x+1]]
 mapvis = lambda ar: '\n'.join([''.join(['\033[91m'+str(c)+'\033[0m' if c > -1 else '\033[94m'+str(c*-1)[-1]+'\033[0m' for c in r[1:-1]]) for r in ar[1:-1]])
