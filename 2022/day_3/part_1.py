5aedf2410785d43645b3fda061c75fd31ca92aca
Cagdas Salur <cagdassalur@gmail.com>
2022-12-03 13:26:53 +0300

    Day 3

---
+++ 2022/day_3/part_1.py
@@ -0,0 +1,10 @@
+bags = open('input').read().strip().split('\n')
+
+chars = '.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+common_score = lambda l: chars.index(set.intersection(*map(set, l)).pop())
+
+bag_compartments = [(bag[:int(len(bag)/2)], bag[int(len(bag)/2):]) for bag in bags]
+print('Part 1: ', sum(map(common_score, bag_compartments)))
+
+elf_groups = [bags[i:i+3] for i in range(0, len(bags), 3)]
+print('Part 2: ', sum(map(common_score, elf_groups)))
