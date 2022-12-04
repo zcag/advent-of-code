9262eea33d4a24a404f997a539d2311c59fae831
Cagdas Salur <cagdassalur@gmail.com>
2022-12-04 14:44:56 +0300

    Refactor to be simpler

--- 2022/day_4/part_1.py
+++ 2022/day_4/part_1.py
@@ -1,9 +1,10 @@
+# Implementation with sets:
 get_range_set = lambda s: set(range(int(s.split('-')[0]), int(s.split('-')[1])+1))
 pairs = [list(map(get_range_set, pair.split(','))) for pair in  open('input').read().strip().split('\n')]
 
-either_subset = lambda pair: pair[0] <= pair[1] or pair[1] <= pair[0]
-print('Part 1: ', len(list(filter(either_subset, pairs))))
+contains = lambda a, b: a <= b or b <= a
+overlaps = lambda a, b: len(a.intersection(b)) != 0
 
-has_intersection = lambda pair: pair[0].intersection(pair[1])
-print('Part 2: ', len(list(filter(has_intersection, pairs))))
+print('Part 1: ', sum(map(contains, *zip(*pairs))))
+print('Part 2: ', sum(map(overlaps, *zip(*pairs))))
 
