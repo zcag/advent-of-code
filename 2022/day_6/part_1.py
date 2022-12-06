3efe67431ce85b44fd645a0b11e52a6c4fbb7fac
Cagdas Salur <cagdassalur@gmail.com>
2022-12-06 08:32:26 +0300

    Day 6

---
+++ 2022/day_6/part_1.py
@@ -0,0 +1,4 @@
+chars = open('input').read().strip()
+unique = lambda chunk: len(set(chunk)) == len(chunk)
+marker_generator = lambda size: ( i+size for i in range(len(chars)-size) if unique(chars[i:i+size]) )
+print('Part 1: ', next(marker_generator(4)), '\nPart 2: ', next(marker_generator(14)))
