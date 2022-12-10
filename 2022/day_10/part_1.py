39b242e7da455effb396a6600ce9d134917136dd
Cagdas Salur <cagdassalur@gmail.com>
2022-12-10 10:58:08 +0300

    Day 10

---
+++ 2022/day_10/part_1.py
@@ -0,0 +1,12 @@
+ops = open('input').read().strip().replace('addx', 'noop\naddx').splitlines()
+
+x, total_strength, crt = 1, 0, []
+for cycle, op in enumerate(ops):
+  if cycle in range(19,221,40): total_strength += (cycle+1) * x
+
+  if cycle%40 == 0: crt.append('\n')
+  crt.append('█' if abs((cycle%40)-x) < 2 else ' ')
+
+  if op != 'noop': x += int(op.split()[1])
+
+print('Part 1: ', total_strength, '\nPart 2:', ''.join(crt))
