03fadc458ea09ce87f2fa1e75e1557b1402bc8ad
Cagdas Salur <cagdassalur@gmail.com>
2022-12-10 12:04:57 +0300

    Change replace

--- 2022/day_10/part_1.py
+++ 2022/day_10/part_1.py
@@ -1,4 +1,4 @@
-ops = open('input').read().strip().replace('addx', 'noop\naddx').splitlines()
+ops = open('input').read().strip().replace('addx', 'noop\n').splitlines()
 
 x, total_strength, crt = 1, 0, []
 for cycle, op in enumerate(ops):
@@ -7,6 +7,6 @@ for cycle, op in enumerate(ops):
   if cycle%40 == 0: crt.append('\n')
   crt.append('█' if abs((cycle%40)-x) < 2 else ' ')
 
-  if op != 'noop': x += int(op.split()[1])
+  if op != 'noop': x += int(op)
 
 print('Part 1: ', total_strength, '\nPart 2:', ''.join(crt))
