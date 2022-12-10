f5c553a8a9906a56bb2c0dba7f3f1fcc1cc9c6e3
Cagdas Salur <cagdassalur@gmail.com>
2022-12-10 13:53:36 +0300

    Use string for crt

--- 2022/day_10/part_1.py
+++ 2022/day_10/part_1.py
@@ -1,12 +1,12 @@
 ops = open('input').read().strip().replace('addx', 'noop\n').splitlines()
 
-x, total_strength, crt = 1, 0, []
+x, total_strength, crt = 1, 0, ''
 for cycle, op in enumerate(ops):
   if cycle in range(19,221,40): total_strength += (cycle+1) * x
 
-  if cycle%40 == 0: crt.append('\n')
-  crt.append('█' if abs((cycle%40)-x) < 2 else ' ')
+  if cycle%40 == 0: crt += '\n'
+  crt += '█' if abs((cycle%40)-x) < 2 else ' '
 
   if op != 'noop': x += int(op)
 
-print('Part 1: ', total_strength, '\nPart 2:', ''.join(crt))
+print('Part 1: ', total_strength, '\nPart 2:', crt)
