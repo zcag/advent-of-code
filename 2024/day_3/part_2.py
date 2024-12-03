ff165a4aeae7eb2ec4cc5b298b082e148884f954
zcag <cagdassalur@gmail.com>
2024-12-03 08:52:04 +0300

    Day 3

---
+++ 2024/day_3/part_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys, re
+from math import prod
+
+pattern, do, res = r'(do(?:n\'t)?\(\))|(?:mul\((\d{1,3}),(\d{1,3})\))', True, 0
+for op in re.findall(pattern, open(sys.argv[1]).read()):
+    if len(op[0]): do = "don't" not in op[0]
+    if len(op[1]) and do: res += prod(map(int, op[1:]))
+print('Part 2:', res)
