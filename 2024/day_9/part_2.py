070ad3e71d1bc7535aab48460e05222076840dcf
zcag <cagdassalur@gmail.com>
2024-12-10 12:41:58 +0300

    Day 9 part 2

--- 2024/day_9/part_2.py
+++ 2024/day_9/part_2.py
@@ -5,31 +5,36 @@ input = list(map(int, open(sys.argv[1]).read().strip()+"0"))
 enumel_as_obj = lambda el: {'id': el[0], 'size': el[1][0], 'free': el[1][1]}
 disk = list(map(enumel_as_obj, enumerate([input[i:i+2] for i in range(0, len(input), 2)])))
 
+diskrepr = lambda disk: ''.join([(str(d['id'])*d['size'])+'.'*d['free'] for d in disk])
+print(diskrepr(disk))
+
 out, id = [], 0
 while id < len(disk) :
-    # if len(out) and out[-1][0] == id: out[-1][1] += disk[id]['size']
-    out.append([id, disk[id]['size']])
+    if len(out) and out[-1]['id'] == id: out[-1]['size'] += disk[id]['size']
+    else: out.append(disk[id])
     disk[id]['size'] = 0
 
-
     for attempt in disk[::-1]:
-        if not attempt['size']: break
         if attempt == disk[id]: break
+        if not attempt['size']: break
         if disk[id]['free'] >= attempt['size']:
-            out.append([attempt['id'], attempt['size']])
+            out.append(attempt)
             disk[id]['free'] -= attempt['size']
             disk.remove(attempt)
             break
             # out.append([disk[-1]['id'], disk[id]['free']])
             # disk[-1]['size'] -= disk[id]['free']
             # break
+    # else: print(outrepr(out))
+    print(diskrepr(out))
     id += 1
 
 
-pos, res = 0, 0
-for id, size in out:
-    for _ in range(size):
-        res += pos * id
-        pos += 1
 
-print('Part 2:', res)
+# pos, res = 0, 0
+# for id, size in out:
+#     for _ in range(size):
+#         res += pos * id
+#         pos += 1
+#
+# print('Part 2:', res)
