5a6295c70f4d78b94e0a4cefd0bb21b0dab9cc49
zcag <cagdassalur@gmail.com>
2024-12-10 12:42:41 +0300

    Day 10

---
+++ 2024/day_10/part_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+grid = [list(map(int, r)) for r in open(sys.argv[1]).read().strip().split("\n")]
+coords = [(x,y) for y in range(len(grid)) for x in range(len(grid[0]))]
+level_coords = {level:{(x,y) for x,y in coords if grid[y][x] == level} for level in range(10)}
+
+bounds = lambda x, y: x in range(len(grid[0])) and y in range(len(grid))
+neighs = lambda x, y: {(x,y) for x,y in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)] if bounds(x,y)}
+valid_neighs = lambda x, y, level: neighs(x, y) & level_coords[level+1]
+
+def walk(x, y, level):
+    if (x,y) in level_coords[9]: return {(x,y)}
+    return set().union(*[walk(*neigh, level+1) for neigh in valid_neighs(x, y, level)])
+
+print('Part 1:', sum([len(walk(x, y, 0)) for x,y in level_coords[0]]))
